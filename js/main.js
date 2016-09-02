54d143b5dbe3082693fc4628ffc5e5d81f685801
anilen <anilen.tp@gmail.com>
2016-09-02 11:35:20 -0300

    add initial files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$('#form').submit(function (e) {
+	e.preventDefault();
+	var valor = $(this).find('input').val();
+	
+
+	if (valor.length < 3) {
+		alert('debes ingresar mas de 3 caracteres');
+	}
+
+	debugger;
+});	
