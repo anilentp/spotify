75b7f4ac34b30b2a4babc663e27740324db7ef0a
anilen <anilen.tp@gmail.com>
2016-09-02 14:15:44 -0300

    add button social network

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@
 $('#form').submit(function (e) {
 	e.preventDefault();
 	var valor = $(this).find('input').val();
-	
+
 
 	if (valor.length < 3) {
 		alert('debes ingresar mas de 3 caracteres');
 	}
-
-	debugger;
 });	
