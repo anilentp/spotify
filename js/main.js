bacf4b242e43bb45626a5507c2140dc4f8cc514c
Matias Bensan <mbensan@gmail.com>
2016-09-03 21:53:49 -0400

    ADD: Spottify quering

--- js/main.js
+++ js/main.js
@@ -3,15 +3,12 @@ var client_id = "16508d14131945baa188c4ec40dd5901";
 var client_secret = "1a41a74419d343ccb72b51302d531980";
 var base64encode = "MTY1MDhkMTQxMzE5NDViYWExODhjNGVjNDBkZDU5MDE6MWE0MWE3NDQxOWQzNDNjY2I3MmI1MTMwMmQ1MzE5ODA="
 
-$('#form').submit(function (e) {
-	e.preventDefault();
-	var valor = $(this).find('input').val();
+// variables de retorno
+var access_token = "";
+var token_type = "";
+var expires_in = "";
 
-	if (valor.length < 3) {
-		alert('debes ingresar mas de 3 caracteres');
-		return;
-	}
-
+function login() {		
 	$.ajax({
 		url: "https://accounts.spotify.com/api/token",
 		type: "POST",
@@ -22,9 +19,51 @@ $('#form').submit(function (e) {
 			grant_type: "client_credentials",
 		}
 	}).done(function(data){
-		debugger;
+
+		access_token = data.access_token;
+		token_type = data.token_type;
+		expires_in = data.expires_in;
+
 	}).fail(function(){
-		console.log('fail');
+
+		alert('Error en comunicacion con servidor');
+		return;
+
+	});
+};
+
+$('#form').submit(function (e) {
+	e.preventDefault();
+	var valor = $(this).find('input').val();
+
+	if (valor.length < 3) {
+		alert('debes ingresar mas de 3 caracteres');
+		return;
+	}
+
+	if (access_token == "") {
+		login();
+	}
+	debugger;
+	$.ajax({
+		url: "https://api.spotify.com/v1/search",
+		type: "GET",
+		headers: {
+			Authorization: "Bearer " + access_token
+		},
+		data: {
+			q: valor,
+			type: "track",
+			market: "CL"
+		}
+	}).done(function(data){
+
 		debugger;
+
+	}).fail(function(){
+
+		alert('Error en comunicacion con servidor');
+		return;
+
 	});
 });	
